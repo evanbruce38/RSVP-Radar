0b833f1981119aa822aa8339866e103f9910c823
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-08 20:54:55 -0500

    added html to home page and changed title in public folder

---
+++ src/pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About(props) {
+    return(
+        <>
+            <section>
+                <h2>Welcome to Travel Manager!</h2>
+            </section>
+
+            <section id='about'>
+                <h2 id="about-title">Plan your dream vacation!</h2>
+
+                <div class='column'>
+
+                </div>
+                <div class="main-section-content">
+                    <p class="home-p">
+                        Just go to search to start looking for hotels in any destination. Then click save to keep track of that your previous search! Head to the Saved Trips page to view your favorites!
+                    </p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Home;
