0b143a7619de1496889b9d559e3494e8e8211fd7
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-17 17:50:09 -0500

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/travel',
+    process.env.MONGODB_URI || 'mongodb+srv://ugabrucer30:Misha398?@cluster0.lfunwdz.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         newUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
     },
 );
 
