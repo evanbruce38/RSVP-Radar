4ed15428b785f2f63022efee4cb76f2fb1674fea
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-17 18:47:43 -0500

    fixing typo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require("apllo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
